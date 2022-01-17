a8a11555cdd13b7d17070c69c1d9d229f946c9d0
Julian Castro <juliancastro@MacBook-Pro.local>
2022-01-17 13:16:52 -0500

    added sign up with email and password

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+
 
 
 class App extends React.Component {
@@ -16,16 +17,28 @@ class App extends React.Component {
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
-unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
-  componentDidMount () {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -33,10 +46,10 @@ unsubscribeFromAuth = null
     this.unsubscribeFromAuth();
   }
 
-  render(){
+  render() {
     return (
       <div>
-        <Header currentUser = {this.state.currentUser} />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
