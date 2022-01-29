c57a9bac0ac5c18f0baacd98d3b3af5c5854706e
Julian Castro <juliancastro@MacBook-Pro.local>
2022-01-29 13:49:47 -0500

    added new shop data and can now see added items in the cart drop down

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.component';
 import './cart-dropdown.styles.scss';
+import CartItem from "../cart-item/cart-item.component";
+import {connect} from 'react-redux';
 
-const CartDropDown = () => (
+const CartDropDown = ({cartItems}) => (
     <div className="cart-dropdown">
         <div className="cart-items" />
+            {
+                cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))}
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropDown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems 
+})
+    
+
+export default connect(mapStateToProps,null)(CartDropDown);
